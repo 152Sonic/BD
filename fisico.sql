d1b7daa8a152f18caeeb021f8917eaa78846c37b
44Mozart <mariaqbarros28@gmail.com>
2020-12-04 00:00:49 +0000

    d

--- fisico.sql
+++ fisico.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Forward Engineering
-
+DROP SCHEMA ESCOLA;
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -33,11 +33,11 @@ CREATE TABLE IF NOT EXISTS `Escola`.`Turma` (
   `idTurma` INT NOT NULL,
   `N_Alunos` INT NOT NULL,
   `Diretor_de_Turma` VARCHAR(45) NOT NULL,
-  `Escola` INT NULL,
+  `EscolaT` INT NULL,
   PRIMARY KEY (`idTurma`),
-  INDEX `Escola_idx` (`Escola` ASC) VISIBLE,
-  CONSTRAINT `Escola`
-    FOREIGN KEY (`Escola`)
+  INDEX `Escola_idx` (`EscolaT` ASC) VISIBLE,
+  CONSTRAINT `EscolaT`
+    FOREIGN KEY (`EscolaT`)
     REFERENCES `Escola`.`Escola` (`idEscola`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -53,17 +53,18 @@ CREATE TABLE IF NOT EXISTS `Escola`.`Aluno` (
   `idade` INT NOT NULL,
   `email` VARCHAR(45) NULL,
   `Turma` INT NULL,
-  `Escola` INT NULL,
+  `EscolaAl` INT NULL,
+  `media` FLOAT NOT NULL,
   PRIMARY KEY (`idAluno`),
-  INDEX `Escola_idx` (`Escola` ASC) VISIBLE,
+  INDEX `Escola_idx` (`EscolaAl` ASC) VISIBLE,
   INDEX `Turma_idx` (`Turma` ASC) VISIBLE,
   CONSTRAINT `Turma`
     FOREIGN KEY (`Turma`)
     REFERENCES `Escola`.`Turma` (`idTurma`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `Escola`
-    FOREIGN KEY (`Escola`)
+  CONSTRAINT `EscolaAl`
+    FOREIGN KEY (`EscolaAl`)
     REFERENCES `Escola`.`Escola` (`idEscola`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -77,11 +78,11 @@ CREATE TABLE IF NOT EXISTS `Escola`.`Docente` (
   `idDocente` INT NOT NULL,
   `nr_de_Contribuinte` VARCHAR(45) NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
-  `Escola` INT NULL,
+  `EscolaD` INT NULL,
   PRIMARY KEY (`idDocente`),
-  INDEX `Escola_idx` (`Escola` ASC) VISIBLE,
-  CONSTRAINT `Escola`
-    FOREIGN KEY (`Escola`)
+  INDEX `Escola_idx` (`EscolaD` ASC) VISIBLE,
+  CONSTRAINT `EscolaD`
+    FOREIGN KEY (`EscolaD`)
     REFERENCES `Escola`.`Escola` (`idEscola`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -92,50 +93,18 @@ ENGINE = InnoDB;
 -- Table `Escola`.`Ensina`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Escola`.`Ensina` (
-  `Turma` INT NOT NULL,
-  `Docente` INT NOT NULL,
-  INDEX `Docente_idx` (`Docente` ASC) VISIBLE,
-  INDEX `Turma_idx` (`Turma` ASC) VISIBLE,
-  CONSTRAINT `Turma`
-    FOREIGN KEY (`Turma`)
+  `TurmaE` INT NOT NULL,
+  `DocenteE` INT NOT NULL,
+  INDEX `Docente_idx` (`DocenteE` ASC) VISIBLE,
+  INDEX `Turma_idx` (`TurmaE` ASC) VISIBLE,
+  CONSTRAINT `TurmaE`
+    FOREIGN KEY (`TurmaE`)
     REFERENCES `Escola`.`Turma` (`idTurma`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `Docente`
-    FOREIGN KEY (`Docente`)
+  CONSTRAINT `DocenteE`
+    FOREIGN KEY (`DocenteE`)
     REFERENCES `Escola`.`Docente` (`idDocente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Escola`.`Disciplina`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Escola`.`Disciplina` (
-  `idDisciplina` INT NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idDisciplina`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Escola`.`Aprende`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Escola`.`Aprende` (
-  `Disciplina` INT NOT NULL,
-  `Aluno` INT NOT NULL,
-  `Nota` DECIMAL(3,1) NOT NULL,
-  INDEX `Disciplina_idx` (`Disciplina` ASC) VISIBLE,
-  INDEX `Aluno_idx` (`Aluno` ASC) VISIBLE,
-  CONSTRAINT `Aluno`
-    FOREIGN KEY (`Aluno`)
-    REFERENCES `Escola`.`Aluno` (`idAluno`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `Disciplina`
-    FOREIGN KEY (`Disciplina`)
-    REFERENCES `Escola`.`Disciplina` (`idDisciplina`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- povoacao.sql
+++ povoacao.sql
@@ -1,82 +1,112 @@
 USE Escola;
 
 INSERT INTO Aluno
-	(nr_de_aluno,nome,idade,media,email,idEscola,idTurma)
+	(nr_de_aluno,nome,idade,email,idEscola,idTurma,media)
     VALUES
-		(3,"Marcelo Alexandre Matos Fonseca Lima",16,9.5,"marceloLima@aejc.com",1,1),
-        (4,"Eduardo Semanas Oliveira Araújo",15,11,"eduHard4@aejc.com",2,4),
-        (9,"Bruno Miguel de Sousa Cancelinha",16,20,"cancelinha9@aejc.com",3,7),
-		(16,"Ricardo Jorge Barroso Certo",16,13.2,"rjbc@aejc.com",1,2),
-        (17,"Bruno Filipe da Silva Ferreira",15,13.6,"brunoFerreira@aejc.com",2,5),
-        (19,"Ricardo Manuel Cerineu Canela",16,10,"ricardo19@aejc.com",3,8),
-		(30,"Filipe Vieira Fortunato",16,10.5,"filipeForunato@aejc.com",1,3),
-        (32,"José Diogo Paiva Bastos",15,16,"jbasto@aejc.com",2,6),
-		(36,"Inês Marques Sampaio",15,18,"marques@aejc.com",3,7),
-        (37,"João Pedro Lima Cardoso Leal",16,12,"jpC@aejc.com",1,1),
-        (39,"Guilherme Vasconcelos da Silva Guerreiro",15,14.5,"gui@aejc.com",2,4),
-        (52,"Pedro Manuel Barbosa de Almeida",16,13.8,"almeida@aejc.com",3,8),
-        (63,"José António Dantas Silva", 15,16,"zeTone63@aejc.com",1,2),
-        (68,"Manuel João Monteiro",16,13.4,"donatelo@aejc.com",2,5),
-        (85,"Júlio Dinis Sá Peixoto",15,12.4,"juliosPeixoto@aejc.com",3,7),
-        (87,"Sara Alexandra Da Silva Pereira",15,16.2,"saraAlex87@aejc.com",1,3),
-        (88,"Marco Matias Pereira Gonçalves",16,19,"marco88@aejc.com",2,6),
-        (105,"Renato Jorge Cruzinha da Silva",15,13,"renatoSilva@aejc.com",3,8),
-        (120,"Rui Filipe Silva Vieira",16,16.4,"ruiVieira@aejc.com",1,1),
-		(122,"Adriana da Santa Guedes",15,17,"candace@aejc.com",2,4),
-        (128,"Adriana Margarida Martins Gonçalves",15,16.1,"adriana@aejc.com",3,7),
-        (136,"Pedro Melo Rocha",17,12.9,"rocha@aejc.com",1,2),
-        (6,"António Alexandre Moreira Lopes", 15, 14.6,"tvi@aejc.com",2,5),
-        (8,"Ricardo Ribeiro Pereira",16,12.6,"blastoise@aejc.com",3,8),
-        (11,"Tiago João Fernandes Baptista",17,10.2,"tijofeba@aejc.com",1,3),
-        (31,"Marco Aurélio Salsa Barbosa",16,13.2,"marcosalsa@aejc.com",2,6),
-        (33,"Ricardo António Gonçalves Pereira",16,18.4,"zagueiro@aejc.com",3,7),
-        (35,"João André Ferreira Aleixo", 15,14.9,"aleixojoao@aejc.com",1,1),
-        (38,"André Almeida Gonçalves",17,12.8,"andresimbs@aejc.com",2,4),
-        (42,"João António Cidade Vieira",17,15.6,"joaocidade42@aejc.com",3,8),
-        (45,"João Paulo Mourão Cabo",15,14.8,"joaoskywalker@aejc.com",1,2),
-        (53,"Bernardo Miguel Teixeira Dias Viseu de Carvalho",16,11.8,"bernardoatlantico@aejc.com",2,5),
-        (55,"Diogo Filipe Lopes Soares",15,11.7,"diogosoares@aejc.com",3,7),
-        (59,"Fernando Manuel de Araújo Pereira",16,9.7,"videirinha@aejc.com",1,3),
-        (102,"Ricardo Guerra Leal",16,10.2,"ricardoleal@aejc.com",2,6),
-        (107,"Nuno Bulhões Melo",17,10.7,"castelinhos@aejc.com",3,8),
-        (123,"Guilherme José Silva Fontes",16,12.3,"everest123@aejc.com",1,1),
-        (127,"José Carlos do Vale Sousa",15,12.7,"pirocarlos@aejc.com",2,4),
-        (129,"Frederico Daniel Pereira Pinto",16,12.9,"fredaniel@aejc.com",3,7),
-        (133,"André Filipe Araújo Pereira de Sousa",15,13.3,"counter-t@aejc.com",1,2),
-        (140,"Marcelo António Caridade Miranda",17,14.0,"chosen1@aejc.com",2,5),
-        (5,"André Miguel Raposo Fonseca",16,15.0,"ofonseca@aejc.com",3,8),
-        (12,"Artur Bernardo da Silva Ribeiro",16,12.0,"arturcigano@aejc.com",1,3),
-        (14,"Davide da Silva Matos",15,14.0,"arisgrooteles@aejc.com",2,6),
-        (24,"Pedro Rafael Bernardo Medeiros",17,17.5,"pedormed@aejc.com",3,7),
-        (25,"Nuno Ricardo Araújo Silva",18,15.2,"nunopedreiro@aejc.com",1,1),
-        (27,"João Duarte Vila Verde Silva",15,12.7,"joaodvvs@aejc.com",2,4),
-        (47,"Shahzod Yusupov",16,14.7,"obalboa@aejc.com",3,8),
-        (51,"Alexandre de Freitas Ferreira Pacheco",15,15.1,"alexpayeco@aejc.com",1,2),
-        (54,"Diogo José Cruz Sobral",16,15.4,"diogoferrero@aejc.com",2,5),
-        (62,"José Pedro Pinto,",17,16.2,"coladix@aejc.com",1,3),
-        (80,"Adriana Henriques Esteves Meireles",17,18.0,"drimeireles@aejc.com",2,6),
-        (81,"Helena Poças Martins",16,18.1,"hpmartins@aejc.com",1,1),
-        (90,"Pedro Miguel Marques Freitas",16,19.0,"putosardz@aejc.com",2,5),
-        (99,"Pedro Mendes Pinto",15,17.1,"gininhas@aejc.com",1,3),
-        (124,"Francisco Jose Alves Laço",16,"xicofontes@aejc.com",2,6)
+		(3,"Marcelo Alexandre Matos Fonseca Lima",16,"marceloLima@aejc.com",1,1,9.5),
+        (4,"Eduardo Semanas Oliveira Araújo",15,"eduHard4@aejc.com",2,4,11.0),
+        (9,"Bruno Miguel de Sousa Cancelinha",16,"cancelinha9@aejc.com",3,7,20.0),
+		(16,"Ricardo Jorge Barroso Certo",16,"rjbc@aejc.com",1,2,13.2),
+        (17,"Bruno Filipe da Silva Ferreira",15,"brunoFerreira@aejc.com",2,5,13.6),
+        (19,"Ricardo Manuel Cerineu Canela",16,"ricardo19@aejc.com",3,8,10.0),
+		(30,"Filipe Vieira Fortunato",16,"filipeForunato@aejc.com",1,3,10.5),
+        (32,"José Diogo Paiva Bastos",15,"jbasto@aejc.com",2,6,16.0),
+		(36,"Inês Marques Sampaio",15,"marques@aejc.com",3,7,18.0),
+        (37,"João Pedro Lima Cardoso Leal",16,"jpC@aejc.com",1,1,12.0),
+        (39,"Guilherme Vasconcelos da Silva Guerreiro",15,"gui@aejc.com",2,4,14.5),
+        (52,"Pedro Manuel Barbosa de Almeida",16,"almeida@aejc.com",3,8,13.8),
+        (63,"José António Dantas Silva", 15,"zeTone63@aejc.com",1,2,16.0),
+        (68,"Manuel João Monteiro",16,"donatelo@aejc.com",2,5,13.4),
+        (85,"Júlio Dinis Sá Peixoto",15,"juliosPeixoto@aejc.com",3,7,12.4),
+        (87,"Sara Alexandra Da Silva Pereira",15,"saraAlex87@aejc.com",1,3,16.2),
+        (88,"Marco Matias Pereira Gonçalves",16,"marco88@aejc.com",2,6,19.0),
+        (105,"Renato Jorge Cruzinha da Silva",15,"renatoSilva@aejc.com",3,8,13.0),
+        (120,"Rui Filipe Silva Vieira",16,"ruiVieira@aejc.com",1,1,16.4),
+		(122,"Adriana da Santa Guedes",15,"candace@aejc.com",2,4,17.0),
+        (128,"Adriana Margarida Martins Gonçalves",15,"adriana@aejc.com",3,7,16.1),
+        (136,"Pedro Melo Rocha",17,"rocha@aejc.com",1,2,12.9),
+        (6,"António Alexandre Moreira Lopes", 15,"tvi@aejc.com",2,5,14.6),
+        (8,"Ricardo Ribeiro Pereira",16,"blastoise@aejc.com",3,8,12.6),
+        (11,"Tiago João Fernandes Baptista",17,"tijofeba@aejc.com",1,3,10.2),
+        (31,"Marco Aurélio Salsa Barbosa",16,"marcosalsa@aejc.com",2,6,13.2),
+        (33,"Ricardo António Gonçalves Pereira",16,"zagueiro@aejc.com",3,7,18.4),
+        (35,"João André Ferreira Aleixo", 15,"aleixojoao@aejc.com",1,1,14.9),
+        (38,"André Almeida Gonçalves",17,"andresimbs@aejc.com",2,4,12.8),
+        (42,"João António Cidade Vieira",17,"joaocidade42@aejc.com",3,8,15.6),
+        (45,"João Paulo Mourão Cabo",15,"joaoskywalker@aejc.com",1,2,14.8),
+        (53,"Bernardo Miguel Teixeira Dias Viseu de Carvalho",16,"bernardoatlantico@aejc.com",2,5,11.8),
+        (55,"Diogo Filipe Lopes Soares",15,"diogosoares@aejc.com",3,7,11.7),
+        (59,"Fernando Manuel de Araújo Pereira",16,"videirinha@aejc.com",1,3,9.7),
+        (102,"Ricardo Guerra Leal",16,"ricardoleal@aejc.com",2,6,10.2),
+        (107,"Nuno Bulhões Melo",17,"castelinhos@aejc.com",3,8,10.7),
+        (123,"Guilherme José Silva Fontes",16,"everest123@aejc.com",1,1,12.3),
+        (127,"José Carlos do Vale Sousa",15,"pirocarlos@aejc.com",2,4,12.7),
+        (129,"Frederico Daniel Pereira Pinto",16,"fredaniel@aejc.com",3,7,12.9),
+        (133,"André Filipe Araújo Pereira de Sousa",15,"counter-t@aejc.com",1,2,13.3),
+        (140,"Marcelo António Caridade Miranda",17,"chosen1@aejc.com",2,5,14.0),
+        (5,"André Miguel Raposo Fonseca",16,"ofonseca@aejc.com",3,8,15.0),
+        (12,"Artur Bernardo da Silva Ribeiro",16,"arturcigano@aejc.com",1,3,12.0),
+        (14,"Davide da Silva Matos",15,"arisgrooteles@aejc.com",2,6,14.0),
+        (24,"Pedro Rafael Bernardo Medeiros",17,"pedormed@aejc.com",3,7,17.5),
+        (25,"Nuno Ricardo Araújo Silva",18,"nunopedreiro@aejc.com",1,1,15.2),
+        (27,"João Duarte Vila Verde Silva",15,"joaodvvs@aejc.com",2,4,12.7),
+        (47,"Shahzod Yusupov",16,"obalboa@aejc.com",3,8,14.7),
+        (51,"Alexandre de Freitas Ferreira Pacheco",15,"alexpayeco@aejc.com",1,2,15.1),
+        (54,"Diogo José Cruz Sobral",16,"diogoferrero@aejc.com",2,5,15.4),
+        (62,"José Pedro Pinto,",17,"coladix@aejc.com",1,3,16.2),
+        (80,"Adriana Henriques Esteves Meireles",17,"drimeireles@aejc.com",2,6,18.0),
+        (81,"Helena Poças Martins",16,"hpmartins@aejc.com",1,1,18.1),
+        (90,"Pedro Miguel Marques Freitas",16,"putosardz@aejc.com",2,5,19.0),
+        (99,"Pedro Mendes Pinto",15,"gininhas@aejc.com",1,3,17.1),
+        (124,"Francisco Jose Alves Laço",16,"xicofontes@aejc.com",2,6,13.0)
 	;
     
 INSERT INTO Docente
-	(idDocente,nr_de_Contribuinte,nome,idTurma,idEscola)
+	(idDocente,nr_de_Contribuinte,nome,idEscola)
     VALUES
-		(1,532467321,"Maria Quintas Barros",1,1),
-        (2,235890472,"Francisco Correia Franco",2,1),
-        (3,173902700,"Ana Teresa Gião Gomes",3,1),
-        (4,356287111,"Maria Beatriz Araujo Lacerda",4,2),
-        (5,801382566,"Luis Enes Sousa",5,2),
-        (6,290172543,"Luis Carlos Salazar Martins",6,2),
-        (7,310200111,"André Carvalho da Cunha Martins",7,3),
-        (8,128356011,"André Gonçalves Vieira",7,3),
-        (9,574123456,"João Figueiredo Peixe Santos",8,3)
+		(1,532467321,"Maria Quintas Barros",1),
+        (2,235890472,"Francisco Correia Franco",1),
+        (3,173902700,"Ana Teresa Gião Gomes",1),
+        (4,356287111,"Maria Beatriz Araujo Lacerda",2),
+        (5,801382566,"Luis Enes Sousa",2),
+        (6,290172543,"Luis Carlos Salazar Martins",2),
+        (7,310200111,"André Carvalho da Cunha Martins",3),
+        (8,128356011,"André Gonçalves Vieira",3),
+        (9,574123456,"João Figueiredo Peixe Santos",3)
 	;
-    
+
+
+INSERT INTO Ensina
+	(idTurma,idDocente)
+    VALUES
+		(1,1),
+        (1,5),
+        (1,9),
+        (2,2),
+        (2,4),
+        (2,1),
+        (3,3),
+        (3,5),
+        (3,6),
+        (4,4),
+        (4,7),
+        (4,8),
+        (5,5),
+        (5,9),
+        (5,1),
+        (6,6),
+        (6,2),
+        (6,3),
+        (7,7),
+        (7,4),
+        (7,5),
+        (8,9),
+        (8,3),
+        (8,5)
+	;
+        
 INSERT INTO Turma
-	(N_Turma,N_Alunos,ID_Diretor,IDEscola)
+	(idTurma,N_Alunos,ID_Diretor,IDEscola)
     VALUES
 		(1,7,1,1),
         (2,6,2,1),
@@ -91,9 +121,9 @@ INSERT INTO Turma
 INSERT INTO Escola
 	(idEscola, Rua, codPostal, Telemovel)
     VALUES
-		(1, "Rua Dona Adelaide", 9650, 294586312),
-        (2, "Rua 25 de Abril", 2845, 236541258),
-        (3, "Rua do Raio Braga", 2654, 298456321)
+		(1, "Rua Dona Adelaide", "4705-231", 294586312),
+        (2, "Rua 25 de Abril", "4750-301", 236541258),
+        (3, "Rua do Raio Braga","4905-082", 298456321)
 	;
     
         
